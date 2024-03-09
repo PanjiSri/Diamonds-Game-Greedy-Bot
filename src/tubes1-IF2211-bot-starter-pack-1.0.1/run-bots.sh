7e144796e1cd83667fa203f8516758f2968a424b
Amalia Putri <amaliaputriii2104@gmail.com>
2024-03-09 19:50:38 +0700

    feat: final

--- src/tubes1-IF2211-bot-starter-pack-1.0.1/run-bots.sh
+++ src/tubes1-IF2211-bot-starter-pack-1.0.1/run-bots.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python main.py --logic Random --email=test@email.com --name=stima --password=123456 --team etimo &
-python main.py --logic Random --email=test1@email.com --name=stima1 --password=123456 --team etimo &
-python main.py --logic Random --email=test2@email.com --name=stima2 --password=123456 --team etimo &
-python main.py --logic Random --email=test3@email.com --name=stima3 --password=123456 --team etimo &
+python main.py --logic Amalia --email=amel@email.com --name=amalia --password=123456 --team etimo &
+python main.py --logic Panji --email=panji@email.com --name=panji --password=123456 --team etimo &
+python main.py --logic Bana --email=bana@email.com --name=bana --password=123456 --team etimo &
+python main.py --logic Random --email=random@email.com --name=random --password=123456 --team etimo &
